68aeb979372793cab97982b3e683ff346f6aac03
hectorsampallo <78111522+hectorsampallo@users.noreply.github.com>
2023-01-27 13:54:31 -0600

    Update streamlit_app.py
    add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinach and Rocket Smoothie')
-streamlit.text('Harded-Boiled Free-Range egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Harded-Boiled Free-Range egg')
+streamlit.text('🥑 Avocado-Toast)
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
