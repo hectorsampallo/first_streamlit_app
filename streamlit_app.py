1997d1b94e9b17777cb3c72c7080988acc8660b9
hectorsampallo <78111522+hectorsampallo@users.noreply.github.com>
2023-01-27 13:51:45 -0600

    Update streamlit_app.py
    add texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach and Rocket Smoothie')
+streamlit.text('Harded-Boiled Free-Range egg')
